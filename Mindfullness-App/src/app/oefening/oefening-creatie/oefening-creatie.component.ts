b640a41467c88f10d189eb3c45672ca85dabb976
Nicolaas Leenknegt <nicolaas.leenknegt.w1123@student.hogent.be>
2018-10-16 18:33:16 +0200

    Semantic toegevoegd aan project en oefening-creatie component aangemaakt. Oefening-creatie component ook direct getest in app.component/html.

---
+++ Mindfullness-App/src/app/oefening/oefening-creatie/oefening-creatie.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-oefening-creatie',
+  templateUrl: './oefening-creatie.component.html',
+  styleUrls: ['./oefening-creatie.component.css']
+})
+export class OefeningCreatieComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
