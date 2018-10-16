d73983c17c0ca9ec4c7b707061fbaa366776c0a7
LeunensMichiel <michiel.leunens.y7743@student.hogent.be>
2018-10-16 23:36:04 +0200

    Angular Material toegevoegd en een sehr mooie sidenav iksee

--- Mindfullness-App/src/app/app.module.ts
+++ Mindfullness-App/src/app/app.module.ts
@@ -4,13 +4,24 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { OefeningCreatieComponent } from './oefening/oefening-creatie/oefening-creatie.component';
 
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule, MatCheckboxModule, MatSidenavModule, MatIconModule, MatToolbarModule, MatListModule} from '@angular/material';
+
+
 @NgModule({
   declarations: [
     AppComponent,
     OefeningCreatieComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
