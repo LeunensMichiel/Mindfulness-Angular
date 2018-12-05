3c6dd88e5b74002b478d11a1fd5b65d00f6f7a4b
Thibaut Maddelein <thibaut.maddelein.y9677@student.hogent.be>
2018-12-05 14:30:17 +0100

    e2e testing - Login - Register

--- cypress/integration/login_page.e2e.spec.ts
+++ cypress/integration/login_page.e2e.spec.ts
@@ -1,24 +1,46 @@
-/// <reference types="Cypress" />
-
-declare namespace Cypress{
-    interface Chainable {
-        login(): void;
-        logout(): void;
-        register(username, password): void;
-        prepTest(): void;
-    }
-}
-
 describe('Login Page', () => {
-    beforeEach(() => {
-        cy.prepTest();
-        cy.register('admin@gmail.com','test');
-        cy.logout();
+
+
+    it('should go to the register page and try to register', () => {
+        cy.visit('http://localhost:4200/register');
+        cy.get('[data-test=email]').type('thibaut@test.be');
+        cy.get('[data-test=password]').type('testtesttest');
+        cy.get('[data-test=passwordControl').type('testtesttest')
+        cy.get('[data-test=registerBtn]').click();
+ 
     });
 
+   it('should go to the login page and try to login', () => {
+       cy.visit('http://localhost:4200/login');
+       cy.get('[data-test=email]').type('thibaut@test.be');
+       cy.get('[data-test=password]').type('testtesttest');
+       cy.get('[data-test=loginBtn]').click();
 
-    it('should go to the login page', () => {
-        cy.visit('');
-    })
+   });
+
+//    it('should show an error when the login is invalid', () => {
+//        cy.visit('');
+
+//        cy.get('[data-test=username]').type('notKwinten');
+//        cy.get('[data-test=password]').type('invalid');
+//        cy.get('[data-test=loginBtn]').click();
+
+//        cy.contains('Error 401 while trying to login user notKwinten');
+//    });
+
+//    it('should not be possible to click the login button when the username is not filled in ', () => {
+//        cy.visit('');
+
+//        cy.get('[data-test=username]').type('notKwinten');
+
+//        cy.get('[data-test=loginBtn]').should('be.disabled');
+//    });
+
+//     it('should not be possible to click the login button when the password is not filled in ', () => {
+//         cy.visit('');
+
+//         cy.get('[data-test=password]').type('invalid');
+
+//         cy.get('[data-test=loginBtn]').should('be.disabled');
+//     });
 });
-
